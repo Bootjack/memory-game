c1443324d4cfa14acb724a91089840a4a50bc086
Jason Hinebaugh <jason.hinebaugh@gmail.com>
2016-05-03 23:23:14 -0400

    Rendering shuffled deck

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ function Card (config) {
     config = config || {};
     this.value = config.value || '';
     this.isFaceUp = false;
-    this.element = document.createElement('div');
+    this.element = config.element || document.createElement('div');
     this.element.addEventListener('click', function (evt) {
         self.clickHandler(evt);
     });
@@ -15,12 +15,13 @@ function Card (config) {
 }
 Card.prototype = {
     clickHandler: function () {
-        this.flip();
+        this.flip().render();
     },
     flip: function (forceDirection) {
         // Flip the card, toggling its visibility
         this.isFaceUp = (undefined === forceDirection ? !this.isFaceUp : !!forceDirection);
-        this.render();
+
+        return this;
     },
     render: function () {
         // Update the element to display the current card state
@@ -29,31 +30,80 @@ Card.prototype = {
             this.element.className += ' is-face-up';
             this.element.textContent = this.value;
         } else {
-            this.element.textContent = '';
+            this.element.innerHTML = '&nbsp;';
         }
+
+        return this;
     }
 };
 
-function Pair () {}
-Pair.prototype = {};
+function Deck (config) {
+    var cards = [],
+        size,
+        value;
 
-function Deck () {}
-Deck.prototype = {};
+    config = config || {};
+    this.size = size = config.size || 8;
+    this.cards = cards;
 
-function Game () {}
+    while (size > 0) {
+        cards.push(new Card({value: size}));
+        cards.push(new Card({value: size}));
+        size -= 1;
+    }
+}
+Deck.prototype = {
+    shuffle: function () {
+        var firstCut,
+            secondCut,
+            i = 0,
+            rand;
+
+        function reverseCardSort (a, b) {
+            return a.value - b.value;
+        }
+
+        while (i < this.size * 4) {
+            rand = Math.floor(Math.random() * this.cards.length);
+            firstCut = this.cards.splice(rand);
+
+            rand = Math.floor(Math.random() * this.cards.length);
+            secondCut = this.cards.splice(rand);
+
+            this.cards = secondCut.concat(this.cards, firstCut);
+            i += 1;
+        }
+
+        return this;
+    }
+};
+
+function Game (config) {
+    config = config || {};
+    this.deck = new Deck();
+    this.element = config.element || document.createElement('div');
+    this.element.className = 'memory-game';
+}
 Game.prototype = {
     start: function () {
-        // TODO: Kick off the gameplay
+        // Kick off the gameplay
+        this.deck.shuffle();
+        this.render();
+
+        return this;
+    },
+    render: function () {
+        var element = this.element;
+
+        this.deck.cards.forEach(function (card) {
+            element.appendChild(card.element);
+            card.render();
+        });
     }
 };
 
 window.addEventListener('load', function () {
     var game = new Game();
+    document.body.appendChild(game.element);
     game.start();
-
-    var card = new Card({
-        value: '7'
-    });
-    document.body.appendChild(card.element);
-    card.render();
 });
